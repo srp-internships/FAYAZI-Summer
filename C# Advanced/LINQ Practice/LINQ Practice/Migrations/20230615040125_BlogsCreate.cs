34d42d9b5a6191cfc0f3ac743839865415388ffd
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-06-15 11:16:37 +0500

    Initial commit

---
+++ C# Advanced/LINQ Practice/LINQ Practice/Migrations/20230615040125_BlogsCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LINQ_Practice.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogsCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ C# Advanced/LINQ Practice/LINQ Practice/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice.Models
+{
+    internal class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
